d87bb6e66f5c77ff4b0266f276ed8cd41233bb03
Abewd <116496332+abewd@users.noreply.github.com>
2023-02-12 23:02:12 +1100

    testing

--- Main/server.js
+++ Main/server.js
@@ -60,8 +60,7 @@ db.connect(function (err) {
   questions();
 });
 
-// questions();
-
+// These are the questions to ask during the prompt
 function questions() {
   inquirer
     .prompt({
@@ -249,6 +248,56 @@ function addRole() {
 
     console.table(res);
     console.log("Department array!");
+
+    promptAddRole(departmentChoices);
+  });
+}
+
+// If you select View All Departments
+function viewDepartments() {
+  console.log("Viewing all departments \n");
+
+  // If you select view "All Employees"
+  var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`;
+  // If there is an error display "err"
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    // If no error, display the table and the script below
+    console.table(res);
+    console.log("All departments viewed!\n");
+    // Run the prompt
+    firstPrompt();
+  });
+}
+
+// If you select Add Department
+
+function addDepartment() {
+  var query = `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    JOIN role r
+    ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const departmentChoices = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`,
+    }));
+
+    console.table(res);
+    console.log("Department added!");
 
     promptAddRole(departmentChoices);
   });
