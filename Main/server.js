b4443933cd1c5593aecce7089bebdb540d347699
Abewd <116496332+abewd@users.noreply.github.com>
2023-02-18 22:25:31 +1100

    adding indexes

--- Main/server.js
+++ Main/server.js
@@ -163,7 +163,7 @@ function questions() {
 function viewEmployee() {
   console.log("A list of all current employees:");
   db.query(
-    "SELECT company.first_name, company.last_name, company.title, company.salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM (SELECT employees.first_name, employees.last_name, employees.manager_id, roles.title, roles.salary FROM employees LEFT JOIN roles ON employees.role_id=roles.id) AS company LEFT JOIN employees as manager ON company.manager_id=manager.id",
+    "SELECT company.first_name ,company.last_name, company.title, company.salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM (SELECT employees.first_name, employees.last_name, employees.manager_id, roles.title, roles.salary FROM employees LEFT JOIN roles ON employees.role_id=roles.id) AS company LEFT JOIN employees as manager ON company.manager_id=manager.id",
     function (error, results) {
       if (error) throw error;
       console.table(results);
@@ -426,9 +426,14 @@ function deleteDepartment() {
 }
 
 function departmentBudget() {
+  inquirer.prompt({
+    message: "Which departments expenditure do you want to look view?",
+    type: "input",
+    name: "id",
+  });
   console.log("Displaying the expenditure for the department:");
   db.query(
-    "SELECT department_id, department.name, SUM(role.salary) AS budget FROM employee LEFT JOIN role on employee.role_id = role_id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;",
+    "SELECT department_id, department.name, SUM(role.salary) AS budget FROM employees LEFT JOIN role on employees.role_id = role_id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;",
     function (error, results) {
       if (error) throw error;
       console.table(results);
